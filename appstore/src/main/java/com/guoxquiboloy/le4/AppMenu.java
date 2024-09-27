1b21391cc805b7fbd2c81e3a81fff18b17a7eb0c
Rin1803 <karrinfrida@gmail.com>
2024-09-28 04:02:55 +0800

    gamez

--- appstore/src/main/java/com/guoxquiboloy/le4/AppMenu.java
+++ appstore/src/main/java/com/guoxquiboloy/le4/AppMenu.java
@@ -2,6 +2,8 @@ package com.guoxquiboloy.le4;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class AppMenu {
@@ -11,5 +13,21 @@ public class AppMenu {
 
     public AppMenu(App app){
         this.app = app;
+    }  
+
+    public Parent getApp(){ 
+        VBox gameContain = new VBox(); 
+
+        Label titleL = new Label(app.getTitle()); 
+        Label pubL = new Label("By: " + app.getPublisher()); 
+        Label rateL = new Label("Rating: " + app.getStar_rating()); 
+        Label genreL = new Label(app.getGenre()); 
+        Label descriL = new Label("Description"+ "\n" + app.getDescription()); 
+        Label downL = new Label("Downloads: " + app.getDownloads()); 
+
+        gameContain.getChildren().addAll(titleL,pubL, genreL, rateL, descriL, downL); 
+        gameContain.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
+        
+        return gameContain; 
     }
 }
--- appstore/src/main/java/com/guoxquiboloy/le4/Main.java
+++ appstore/src/main/java/com/guoxquiboloy/le4/Main.java
@@ -18,17 +18,41 @@ import java.io.IOException;
 public class Main extends Application {
 
     //Scene to ng buong app, scene.setroot niyo nalang kung magswiswitch lang
-    private Scene scene;
+    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
         StoreMenu storeMenu = new StoreMenu();
+
+        //hardcode bla bla kill me now hehhehe 
+        HBox setGame = new HBox(); 
+
+        App[] games = new App[] {
+            new App("CraftMine", "Jangmo", "Fantasy", "Explore the world of CraftMine", 4, 100, "Mapua.png"),
+            new App("Stardew Valley", "Concered Ape", "Fantasy", "Explore the world of Stradew Valley", 5,1000000, "Mapua.png")
+        }; 
+
+        for (App game : games) {
+            AppMenu appSetOne = new AppMenu(game);
+            Parent gamerz = appSetOne.getApp();
+            setGame.getChildren().add(gamerz);
+        }
+
+        VBox gameLayout = (VBox) storeMenu.getParent();
+        gameLayout.getChildren().add(2, setGame);
+
         scene = new Scene(storeMenu.getParent(), 600, 600);
         stage.setScene(scene);
         stage.show();
     }
 
     //Needed Code, pang switch ng scene
+
+
+    public static void switchToAppMenu(App app) {
+        AppMenu appMenu = new AppMenu(app);
+        scene.setRoot(appMenu.getApp());
+    }
 
     public static void main(String[] args) {
         launch();
