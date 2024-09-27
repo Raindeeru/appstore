90e490232a0f857906d65470f5ba3841b07c0903
Raindeeru <derv.gonzales17@gmail.com>
2024-09-27 17:43:19 +0800

    aaa

---
+++ appstore/src/main/java/com/guoxquiboloy/le4/App.java
@@ -0,0 +1,13 @@
+package com.guoxquiboloy.le4;
+
+public class Application {
+    String title;
+    String publisher;
+    String genre;
+    String description;
+    float star_rating;
+    int downloads;
+
+    
+    
+}
